9d7021a5d538e7eb4bb0b60bb7e727fa3865ade2
Will Medina <bugs@willmedina.me>
2024-06-01 03:00:43 -0500

    Ajustes con la sesion
    Ajustando la sesion, el logueo y el deslogueo. Nuevo endpoint: /login/debug_infosesion

--- APPLAGA_APIWEB/m/modelo.php
+++ APPLAGA_APIWEB/m/modelo.php
@@ -4,15 +4,15 @@ interface modelo
 {
 
     const DEFULT_THEME = 'default_theme';
-    const URL = 'https://applaga.net';
-    const URL_CLEAN = 'applaga.net';
-    const FOLDER = '/home/bunkernorte/web/applaga.net';
+    const URL = 'https://beta.applaga.net';
+    const URL_CLEAN = 'beta.applaga.net';
+    const FOLDER = '/home/bunkernorte/web/beta.applaga.net';
     const COMPRESION_HTML = TRUE;
     const STATICVERSIONS = FALSE;
-    const FOLDER_DOCS = '/home/bunkernorte/web/applaga.net/static';
-    const FOLDER_LOG = '/home/bunkernorte/web/applaga.net/log';
-    const FOLDER_ACCESS = '/home/bunkernorte/web/applaga.net/acceso';
-    const DB_INI = '/home/bunkernorte/web/applaga.net/config_db.ini';
+    const FOLDER_DOCS = '/home/bunkernorte/web/beta.applaga.net/static';
+    const FOLDER_LOG = '/home/bunkernorte/web/beta.applaga.net/log';
+    const FOLDER_ACCESS = '/home/bunkernorte/web/beta.applaga.net/acceso';
+    const DB_INI = '/home/bunkernorte/web/beta.applaga.net/config_db.ini';
     const COOKIE_DATA = [
         'name' => 'hash_sesion',
         'path' => '/',
