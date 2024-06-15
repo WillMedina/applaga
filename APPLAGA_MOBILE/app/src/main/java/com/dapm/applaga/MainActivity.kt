cd1eae6920d1f722b029c9cd38e4625048dd5aaf
Deybi Ruiz Marquina <57965724+DeybiJunior@users.noreply.github.com>
2024-06-15 01:19:01 -0500

    Material Switch
    se agrego un diseño de material desing a la switch

--- APPLAGA_MOBILE/app/src/main/java/com/dapm/applaga/MainActivity.kt
+++ APPLAGA_MOBILE/app/src/main/java/com/dapm/applaga/MainActivity.kt
@@ -1,14 +1,6 @@
 package com.dapm.applaga
 
-import android.content.Context
-import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.navigationrail.NavigationRailView
-import okhttp3.*
-import org.json.JSONObject
-import java.io.IOException
-
 class MainActivity : AppCompatActivity() {
 
 }
