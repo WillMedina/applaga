306da0dabfaf5fdfe97cb08ee689979e8240fa36
Deybi Ruiz Marquina <57965724+DeybiJunior@users.noreply.github.com>
2024-06-18 19:40:28 -0500

    boton retroceso de Detalles del Punto Insectos
    se agrego el boton retroceso al activity PuntoInsectoOperarioActivity

--- APPLAGA_MOBILE/app/src/main/java/com/dapm/applaga/PuntoInsectoOperarioActivity.kt
+++ APPLAGA_MOBILE/app/src/main/java/com/dapm/applaga/PuntoInsectoOperarioActivity.kt
@@ -1,4 +1,37 @@
 package com.dapm.applaga
 
-class PuntoInsectoOperarioActivity {
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+
+class PuntoInsectoOperarioActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_punto_operario)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        supportActionBar?.title = "Detalles del Punto Insectos"
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
