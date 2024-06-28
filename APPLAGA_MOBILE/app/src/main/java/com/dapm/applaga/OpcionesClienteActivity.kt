f97822b42f620fb9086a93932cec4d982fbbee4d
Deybi Ruiz Marquina <57965724+DeybiJunior@users.noreply.github.com>
2024-06-28 01:47:13 -0500

    Update OpcionesClienteActivity.kt

--- APPLAGA_MOBILE/app/src/main/java/com/dapm/applaga/OpcionesClienteActivity.kt
+++ APPLAGA_MOBILE/app/src/main/java/com/dapm/applaga/OpcionesClienteActivity.kt
@@ -1,16 +1,22 @@
 package com.dapm.applaga
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.MenuItem
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class OpcionesClienteActivity : AppCompatActivity() {
+    private lateinit var switchNotificacion: SwitchMaterial
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_opciones_cliente)
 
-        // Configurar la barra de herramientas
+        // Configurar la barra de herramientas y otros elementos si es necesario
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
@@ -20,6 +26,27 @@ class OpcionesClienteActivity : AppCompatActivity() {
 
         // Configurar el título de la barra de herramientas
         supportActionBar?.title = "Opciones Cliente"
+
+        // Obtener referencia al switch desde el layout
+        switchNotificacion = findViewById(R.id.switchnotificacion)
+
+        // Inicializar SharedPreferences
+        sharedPreferences = getSharedPreferences("AppPrefs", Context.MODE_PRIVATE)
+
+        // Obtener el estado guardado del switch desde SharedPreferences
+        val voiceNotificationsEnabled = sharedPreferences.getBoolean("voice_notifications_enabled", false)
+
+        // Establecer el estado actual del switch
+        switchNotificacion.isChecked = voiceNotificationsEnabled
+
+        // Ejemplo de cómo usar el switch
+        switchNotificacion.setOnCheckedChangeListener { _, isChecked ->
+            // Guardar el estado del switch en SharedPreferences al cambiar
+            with(sharedPreferences.edit()) {
+                putBoolean("voice_notifications_enabled", isChecked)
+                apply()
+            }
+        }
     }
 
     // Manejar el botón de retroceso
